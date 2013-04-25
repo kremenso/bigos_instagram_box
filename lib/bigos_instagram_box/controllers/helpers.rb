6cf01545e4322b4718ffeae98ec6be56879b5925
kremenso <kremenso@o2.pl>
2013-04-25 21:58:21 +0200

    repeat untill count or nil next page

--- lib/bigos_instagram_box/controllers/helpers.rb
+++ lib/bigos_instagram_box/controllers/helpers.rb
@@ -3,32 +3,41 @@ module BigosInstagramBox
     module Helpers
       extend ActiveSupport::Concern
 
-      def bigos_instagram_box_widget(max_tag_id = nil, count = 120)
+      def bigos_instagram_box_widget(max_tag_id = nil, count = 120, posts = [])
 
         Instagram.configure do |config|
           config.client_id = BigosInstagramBox.client_id
           config.client_secret = BigosInstagramBox.client_secret
         end
 
-        posts = []
-        get_by_hash(posts, max_tag_id, count) unless BigosInstagramBox.hashtag.blank?
+        get_by_hash(posts, max_tag_id, count)unless BigosInstagramBox.hashtag.blank?
         get_by_user(posts, max_tag_id, count) unless BigosInstagramBox.user_id.blank?
         search_and_get_by_user_name(posts, max_tag_id, count) unless BigosInstagramBox.user_name.blank?
         get_by_location(posts, max_tag_id, count) unless BigosInstagramBox.location.blank?
 
+
         render :partial=>"bigos_instagram_box/module/widget", :locals=>{:posts => posts}
+
       end
 
-      def get_by_user_id(posts, max_tag_id, count, id)
+      def get_by_user_id(posts, max_tag_id, count, id, pages = [])
         begin
           items = Instagram.user_recent_media(
               id,
-              :max_tag_id => max_tag_id,
+              :max_id => max_tag_id,
               :count => count,
               :access_token => BigosInstagramBox.access_token
           )
+          next_id = items.pagination.next_max_id
+
           items.each do |photo_hash|
             posts << BigosInstagramBox::InstagramPhoto.new(photo_hash)
+          end
+
+          if (posts.length < count) && !next_id.nil? && !pages.include?(next_id)
+            puts "MAX TAG ID" + next_id
+            pages << next_id
+            get_by_user_id(posts, next_id, count, id, pages)
           end
         rescue
           Rails.logger.error("Instagram by User ID API ERROR")
@@ -53,10 +62,11 @@ module BigosInstagramBox
       def get_by_hash(posts, max_tag_id, count)
         begin
           items = Instagram.tag_recent_media(
-            BigosInstagramBox.hashtag,
-           :max_tag_id => max_tag_id,
-           :count => count
+              BigosInstagramBox.hashtag,
+              :max_tag_id => max_tag_id,
+              :count => count
           )
+
           items.data.each do |photo_hash|
             posts << BigosInstagramBox::InstagramPhoto.new(photo_hash)
           end
